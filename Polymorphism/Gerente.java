0f77d17b34623113d66fbd3766c7102713d42346
Lucas Smaniotto <lucas91755@gmail.com>
2022-08-05 10:42:24 -0300

    Introdução a Herança

---
+++ Polymorphism/Gerente.java
@@ -0,0 +1,21 @@
+//Gerente herda da classe Funcionario
+public class Gerente extends Funcionario{
+    
+    private int senha;
+
+    public boolean autenticaSenha(int senha){
+        if(this.senha == senha)
+            return true;
+        return false;
+    }
+
+    public void setSenha(int senha){
+        this.senha = senha;
+    }
+    
+    /*
+    public double getBonus(){
+        return this.salario;
+    }
+     */ 
+}
---
+++ Polymorphism/TestaGerente.java
@@ -0,0 +1,18 @@
+public class TestaGerente {
+    
+    public static void main(String[] args) {
+        Gerente gerente = new Gerente();
+        gerente.setNome("Aquele");
+        gerente.setCPF("789.546.548-98");
+        gerente.setSalario(5000.0);
+
+        System.out.println(gerente.getNome());
+        System.out.println(gerente.getCPF());
+        System.out.println(gerente.getSalario());
+
+        gerente.setSenha(1222);
+        
+        boolean autenticou = gerente.autenticaSenha(1222);
+        System.out.println(autenticou);
+    }
+}
