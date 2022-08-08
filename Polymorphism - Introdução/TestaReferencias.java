7398dd889a6d71f8dc077ee4f1b0198a9fee2458
Lucas Smaniotto <lucas91755@gmail.com>
2022-08-08 09:56:10 -0300

    Introdução a Polimorfismo

--- Polymorphism - Introdução/TestaReferencias.java
+++ Polymorphism - Introdução/TestaReferencias.java
@@ -6,10 +6,6 @@ public class TestaReferencias {
         g1.setNome("Lucas");
         g1.setSalario(5000.0);
 
-        Funcionario f1 = new Funcionario();
-        f1.setNome("Licas");
-        f1.setSalario(2000.0);
-
         Funcionario editor = new EditorDeVideo();
         editor.setNome("Felipe");
         editor.setSalario(2500.0);
@@ -20,7 +16,6 @@ public class TestaReferencias {
         
         ControleBonus controle = new ControleBonus();
         controle.registra(g1);
-        controle.registra(f1);
         controle.registra(editor);
         controle.registra(designer);
 
