23d1b872910c33202c5f7c40659871d49f5ca268
Lucas Smaniotto <lucas91755@gmail.com>
2022-08-16 12:35:19 -0300

    Organização de Classes com Pacotes

---
+++ Packages - Organizando classes com Pacotes/src/br/com/bytebank/banco/modelo/SaldoInsuficienteException.java
@@ -0,0 +1,17 @@
+package br.com.bytebank.banco.modelo;
+// CHECKED
+public class SaldoInsuficienteException extends Exception{
+
+    public SaldoInsuficienteException(String msg) {
+        super(msg);
+    }
+    
+}
+
+/* UNCHECKED
+public class SaldoInsuficienteException extends RuntimeException{
+    public SaldoInsuficienteException(String msg) {
+        super(msg);
+    } 
+}
+*/
---
+++ Packages - Organizando classes com Pacotes/src/br/com/bytebank/banco/teste/TesteTributaveis.java
@@ -0,0 +1,21 @@
+package br.com.bytebank.banco.teste;
+
+import br.com.bytebank.banco.modelo.CalculadorDeImposto;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.SeguroDeVida;
+
+public class TesteTributaveis {
+    
+    public static void main(String[] args) {
+        ContaCorrente cc = new ContaCorrente(222, 333);
+        cc.deposita(100.0);
+
+        SeguroDeVida sv = new SeguroDeVida();
+
+        CalculadorDeImposto calc = new CalculadorDeImposto();
+        calc.registra(cc);
+        calc.registra(sv);
+
+        System.out.println(calc.getTotalImposto());
+    }
+}
