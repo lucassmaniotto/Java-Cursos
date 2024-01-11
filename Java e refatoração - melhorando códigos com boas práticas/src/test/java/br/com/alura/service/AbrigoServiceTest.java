2d4a607a2ff2ce9b3c9c13682dda4ba978bde423
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2024-01-11 11:50:28 -0300

    chore: resolvendo warnings

--- Java e refatoração - melhorando códigos com boas práticas/src/test/java/br/com/alura/service/AbrigoServiceTest.java
+++ Java e refatoração - melhorando códigos com boas práticas/src/test/java/br/com/alura/service/AbrigoServiceTest.java
@@ -14,6 +14,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@SuppressWarnings("unchecked")
 public class AbrigoServiceTest {
 
     private ClientHttpConfiguration client = mock(ClientHttpConfiguration.class);
