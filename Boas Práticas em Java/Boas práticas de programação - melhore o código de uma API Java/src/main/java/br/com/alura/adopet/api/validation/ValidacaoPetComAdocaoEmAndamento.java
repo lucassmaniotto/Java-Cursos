ad966b9e02c907a4322ad20aa060d3ec095385e0
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2024-02-01 17:39:47 -0300

    refactor: otimizando consultas

--- Boas Práticas em Java/Boas práticas de programação - melhore o código de uma API Java/src/main/java/br/com/alura/adopet/api/validation/ValidacaoPetComAdocaoEmAndamento.java
+++ Boas Práticas em Java/Boas práticas de programação - melhore o código de uma API Java/src/main/java/br/com/alura/adopet/api/validation/ValidacaoPetComAdocaoEmAndamento.java
@@ -1,34 +1,27 @@
 package br.com.alura.adopet.api.validation;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
 import br.com.alura.adopet.api.exception.ValidacaoException;
-import br.com.alura.adopet.api.model.Adocao;
-import br.com.alura.adopet.api.model.Pet;
 import br.com.alura.adopet.api.model.StatusAdocao;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
-import br.com.alura.adopet.api.repository.PetRepository;
 
 @Component
 public class ValidacaoPetComAdocaoEmAndamento implements ValidacaoSolicitacaoAdocao {
     @Autowired
     private AdocaoRepository adocaoRepository;
-
-    @Autowired
-    private PetRepository petRepository;
     
-    public void validar(SolicitacaoAdocaoDto dto) {        
-        List<Adocao> adocoes = adocaoRepository.findAll();
-        Pet pet = petRepository.getReferenceById(dto.petId());
+    public void validar(SolicitacaoAdocaoDto dto) {
 
-        for (Adocao a : adocoes) {
-            if (a.getPet() == pet && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
+        boolean petTemAdocaoEmAndamento = adocaoRepository
+                    .existsByPetIdAndStatus(dto.petId(), StatusAdocao.AGUARDANDO_AVALIACAO);
+                    
+        if (petTemAdocaoEmAndamento) {
                 throw new ValidacaoException("Pet já está aguardando avaliação para ser adotado!");
-            }
         }
+        
     }
 }
