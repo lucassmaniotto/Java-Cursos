d4a1e8e23cea996dd2e16893e252601c8fdaceeb
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2024-02-01 11:06:07 -0300

    refactor: ajustando validações do DTO

--- Boas Práticas em Java/Boas práticas de programação - melhore o código de uma API Java/src/main/java/br/com/alura/adopet/api/dto/ReprovacaoAdocaoDto.java
+++ Boas Práticas em Java/Boas práticas de programação - melhore o código de uma API Java/src/main/java/br/com/alura/adopet/api/dto/ReprovacaoAdocaoDto.java
@@ -1,6 +1,12 @@
 package br.com.alura.adopet.api.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record ReprovacaoAdocaoDto(
+    @NotNull
     Long adocaoId,
+    
+    @NotBlank
     String justificativa
 ) {}
--- Boas Práticas em Java/Boas práticas de programação - melhore o código de uma API Java/src/main/java/br/com/alura/adopet/api/dto/SolicitacaoAdocaoDto.java
+++ Boas Práticas em Java/Boas práticas de programação - melhore o código de uma API Java/src/main/java/br/com/alura/adopet/api/dto/SolicitacaoAdocaoDto.java
@@ -1,7 +1,15 @@
 package br.com.alura.adopet.api.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record SolicitacaoAdocaoDto(
+    @NotNull
     Long tutorId,
+
+    @NotNull
     Long petId,
+
+    @NotBlank
     String motivo
 ) {}
